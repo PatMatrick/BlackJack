462c7512475c5a21b0406b10914ce0a71295e1de
matthewpatrick <mpatrick9376@gmail.com>
2020-10-28 23:58:52 -0500

    Adding to the card directory.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 ###HELLO
 
-cards = {Ace_Club: 10}
+cards = {"Ace_Club": 10, "Ace_Heart": 10, "Ace_Spade": 10, "Ace_Diamond": 10, "King_Club": 10, "King_Heart": 10,
+         "King_Spade": 10, "King_Diamond": 10, "Queen_Club": 10, "Queen_Heart": 10, "Queen_Spade": 10, "Queen_Diamond": 10,
+         "Jack_Club": 10, "Jack_Heart": 10, "Jack_Spade": 10, "Jack_Diamond": 10, "10_Club": 10, "10_Heart": 10, "10_Spade": 10,
+         "10_Diamond": 10, "9_Club": 9, "9_Heart": 9, "9_Spade": 10, "9_Diamond": 10,}
+
+class Player
