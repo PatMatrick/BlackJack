1f7ada2dfda27d61d8fcd69577d7f04945911d5b
matthewpatrick <mpatrick9376@gmail.com>
2020-10-29 01:50:11 -0500

    1. Finished card directory 2. Started Player Class 3. Main Menu 4. Began game logic

--- main.py
+++ main.py
@@ -3,6 +3,32 @@
 cards = {"Ace_Club": 10, "Ace_Heart": 10, "Ace_Spade": 10, "Ace_Diamond": 10, "King_Club": 10, "King_Heart": 10,
          "King_Spade": 10, "King_Diamond": 10, "Queen_Club": 10, "Queen_Heart": 10, "Queen_Spade": 10, "Queen_Diamond": 10,
          "Jack_Club": 10, "Jack_Heart": 10, "Jack_Spade": 10, "Jack_Diamond": 10, "10_Club": 10, "10_Heart": 10, "10_Spade": 10,
-         "10_Diamond": 10, "9_Club": 9, "9_Heart": 9, "9_Spade": 10, "9_Diamond": 10,}
+         "10_Diamond": 10, "9_Club": 9, "9_Heart": 9, "9_Spade": 9, "9_Diamond": 9, "8_Club": 8, "8_Heart": 8,
+         "8_Spade": 8, "8_Diamond": 8, "7_Club": 7, "7_Heart": 7, "7_Spade": 7, "7_Diamond": 7, "6_Club": 6, "6_Heart": 6,
+         "6_Spade": 6, "6_Diamond": 6, "5_Club": 5, "5_Heart": 5, "5_Spade": 5, "5_Diamond": 5, "4_Club": 4,
+         "4_Heart": 4, "4_Spade": 4, "4_Diamond": 4, "3_Club": 3, "3_Heart": 3, "3_Spade": 3, "3_Diamond": 3,
+         "2_Club": 2, "2_Heart": 2, "2_Spade": 2, "2_Diamond": 2}
 
-class Player
+class Player:
+    def __init__(self, name):
+        self.currentscore = 0
+        self.chips = 0
+
+
+
+
+def start():
+    print("Hello and welcome to Blackjack!")
+    print("1.) Begin game")
+    print("2.) Quit")
+    option = input("-> ")
+    if option.strip() == "1":
+        game()
+    if option.strip() == "2":
+        sys.exit()
+
+
+def game():
+
+
+start()
